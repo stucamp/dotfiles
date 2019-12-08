3f4d9685dfdc9df1db7c801ac4de9673013f4b1d
Stu Campbell <stucampbell.git@gmail.com>
2019-12-08 10:57:39 -0700

    some nix stuff/temps

---
+++ nix/gcc/default.nix
@@ -0,0 +1,14 @@
+
+with import <nixpkgs> {}; {
+    nothingEnv = gcc8Stdenv.mkDerivation {
+        name = "nothing-env";
+        buildInputs = [ stdenv
+                        gcc
+                        gdb
+                        SDL2
+                        pkgconfig
+                        cmake
+                        valgrind
+                      ];
+    };
+}
