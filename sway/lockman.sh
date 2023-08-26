461e8c201e9bf0f8783856855813e773b0953a2d
Stu Campbell <stucampbell.git@gmail.com>
2023-08-26 02:35:02 -0700

    sync laptop

--- sway/lockman.sh
+++ sway/lockman.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 # Times the screen off and puts it to background
 swayidle \
-    timeout 10 'swaymsg "output * dpms off"' \
+    timeout 5 'swaymsg "output * dpms off"' \
     resume 'swaymsg "output * dpms on"' &
 # Locks the screen immediately
+swayidlepid=$!
 swaylock -c 000000
 # Kills last background task so idle timer doesn't keep running
-kill %%
+kill $swayidlepid
