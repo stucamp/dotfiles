9bfd9f660572fabd60890a56d137092b3830892c
Stu Campbell <stucampbell.git@gmail.com>
2023-08-26 17:43:59 -0700

    more tinkering

---
+++ setup/install.sh
@@ -0,0 +1,102 @@
+###############################################################################
+# CLI
+###############################################################################
+
+bat,
+btrfs-progs,
+build-essential,
+clang,
+clipman,
+debhelper,
+devscipts,
+dh-make,
+duf,
+entr,
+exa,
+exfatprobs,
+f2fs-tools,
+fdupes,
+ffmpeg,
+figlet,
+fzf,
+g++,
+gcc,
+gdb,
+git,
+htop,
+hyperfine,
+lintian,
+luajit,
+luarocks,
+mc,
+neofetch,
+nfs-common,
+ntfs-3g,
+pandoc,
+parallel,
+python3-venv,
+ripgrep,
+rsync,
+taskwarrior,
+tldr,
+tmux,
+toilet,
+valgrind,
+vim,
+zip,
+zoxide,
+zsh,
+
+###############################################################################
+# WM
+###############################################################################
+
+clipman,
+dex,
+flatpak,
+fonts-anonymous-pro,
+fonts-firecode,
+fonts-font-awesome,
+fonts-recommended,
+grim,
+grimshot,
+mako-notifier,
+sway,
+sway-backgrounds,
+sway-notification-center,
+swaybg,
+swayidle,
+swayimg,
+swaykbdd,
+swaylock,
+waybar,
+wofi,
+xdg-desktop-portal-wlr,
+xwayland,
+
+###############################################################################
+# GUI
+###############################################################################
+
+cheese,
+firefox-esr,
+vlc,
+zathura,
+
+###############################################################################
+# Downloads
+###############################################################################
+
+#wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+#https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+
+###############################################################################
+# TODO
+###############################################################################
+#
+# oh-my-zsh
+# nvm
+# Go
+# Rust
+# Lua
+#
